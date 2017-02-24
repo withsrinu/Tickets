2c5e751f37e6cc31b0a3baa54e55cc5886aca94d
lankireddy <jagan4189@gmail.com>
2017-02-24 18:24:21 +0530

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+	before_action :find_post, only:[:show, :edit, :update, :destory]
+  def index
+  @posts=Post.all.order("created_at DESC")
+ end
+
+  def new
+  	@post=Post.new
+ end
+ def edit
+ end
+
+  def create 
+  	@post=Post.new(post_params)
+  	if @post.save
+  		redirect_to @post
+  	else 
+  		render 'new'
+  	end 
+ end 
+
+  def show
+  	@post=Post.find(params[:id])
+ end
+  def update
+  	@post=Post.find(params[:id])
+     if @post.update(post_params)
+     	redirect_to @post
+     else
+     	render 'edit'
+     end 
+ end
+ 
+ def destory
+  @post.destory
+  redirect_to root_path
+ end
+
+ private
+
+ def find_post
+ 	@post=Post.find(params[:id])
+ end 
+ def post_params
+ 	params.require(:post).permit(:title, :body)
+  end
+
+end
